da0bdb1da60ae4fe58db7eb054a276f2508cd8eb
Luke Ganzevoort <Luke Ganzevoort>
2017-03-06 16:08:24 -0500

    arc function draws complete circle

--- Project02_all_together/vgademo4_all.srcs/sources_1/new/draw_arc.vhd
+++ Project02_all_together/vgademo4_all.srcs/sources_1/new/draw_arc.vhd
@@ -41,7 +41,6 @@ architecture Behavioral of Green_cube is
 signal Object_X_pos : STD_LOGIC_VECTOR(10 downto 0) := B"00000011110"; -- Decimal 30
 signal Object_Y_pos : STD_LOGIC_VECTOR(10 downto 0) := B"00110011010"; -- Decimal 410
 
-  function  sqrt  ( d : UNSIGNED ) return UNSIGNED;
   function  sqrt  ( d : UNSIGNED ) return UNSIGNED is
     variable a : unsigned(31 downto 0):= d;  --original input.
     variable q : unsigned(15 downto 0):=(others => '0');  --result.
@@ -49,72 +48,49 @@ signal Object_Y_pos : STD_LOGIC_VECTOR(10 downto 0) := B"00110011010"; -- Decima
     variable i : integer:=0;
   begin
     for i in 0 to 15 loop
-    right(0):='1';
-    right(1):=r(17);
-    right(17 downto 2):=q;
-    left(1 downto 0):=a(31 downto 30);
-    left(17 downto 2):=r(15 downto 0);
-    a(31 downto 2):=a(29 downto 0);  --shifting by 2 bit.
-    if ( r(17) = '1') then
-    r := left + right;
-    else
-    r := left - right;
-    end if;
-    q(15 downto 1) := q(14 downto 0);
-    q(0) := not r(17);
+      right(0):='1';
+      right(1):=r(17);
+      right(17 downto 2):=q;
+      left(1 downto 0):=a(31 downto 30);
+      left(17 downto 2):=r(15 downto 0);
+      a(31 downto 2):=a(29 downto 0);  --shifting by 2 bit.
+      if ( r(17) = '1') then
+        r := left + right;
+      else
+        r := left - right;
+      end if;
+      q(15 downto 1) := q(14 downto 0);
+      q(0) := not r(17);
     end loop;
     return q;
   end sqrt;
 
 begin
+  draw_arc:process(hcount,vcount,blank)       -- Procedural block for displaying the GREEN object
+    variable row : integer := 0;
+    variable col : integer := 0;
+    variable number : integer := 0;
+    variable Ecol1, Ecol2, Erow1, Erow2 : integer := 0;
+    constant radiusSq : integer := (20*20);
+    constant centerX : integer := 260;
+    constant centerY : integer := 180;
 
+  begin
+    row := conv_integer(vcount);
+    col := conv_integer(hcount);
+    Ecol1 := centerX + TO_INTEGER(sqrt(TO_UNSIGNED((radiusSq-(row-centerY)*(row-centerY)),32)));
+    Ecol2 := centerX - TO_INTEGER(sqrt(TO_UNSIGNED((radiusSq-(row-centerY)*(row-centerY)),32)));
+    Erow1 := centerY + TO_INTEGER(sqrt(TO_UNSIGNED((radiusSq-(col-centerX)*(col-centerX)),32)));
+    Erow2 := centerY - TO_INTEGER(sqrt(TO_UNSIGNED((radiusSq-(col-centerX)*(col-centerX)),32)));
 
-
- process(hcount,vcount,blank)       -- Procedural block for displaying the GREEN object
-   variable row : integer := 0;
-   variable col : integer := 0;
-   variable number : integer := 0;
-   variable num : integer := 0;
-   variable num1 : integer := 0;
-
- begin
-
-   row := conv_integer(vcount);
-   col := conv_integer(hcount);
-   num := (2500-(row-240)*(row-240));
-   num1 := TO_INTEGER(sqrt(TO_UNSIGNED(num,32)));
-   number := 320 + num1;
-
-   if(col = number)  and (blank = '0') then
+    if(col = Ecol1 or row = Erow1 or col = Ecol2 or row = Erow2) and (blank = '0') then
      Green <= X"F";
-   else
+    else
      Green <= X"0";
-   end if;
- end process;
-
--- -------------------------------------------------------------------------
- process(VS, reset) -- Moving the Red object from left to right and back on Vertical sync
- begin
-  if(reset = '1') then
-     Object_X_pos <= B"00000011110";    -- Decimal 30
-  elsif(rising_edge(VS)) then
-   if(right_btn = '1') then
-    if((640 - Object_X_pos) > 60) then 	    -- Move from L -> R
-   	    Object_X_pos <= Object_X_pos + 5;
- 	elsif(Object_X_pos = 580) then			-- Reach extreme RIGHT POSITION
-   	    Object_X_pos <= Object_X_pos;
-   	end if;
-   elsif(left_btn = '1') then
- 	if((Object_X_pos - 0) > 30) then		-- Move from R -> L
-        Object_X_pos <= Object_X_pos - 5;
- 	elsif(Object_X_pos = 30) then 			-- Reach extreme LEFT POSITION
-        Object_X_pos <= Object_X_pos;
- 	end if;
-   end if;      -- btn
-  end if;       -- rising_edge
+    end if;
   end process;
 
-Red <= X"0";
-Blue <= X"0";
+  Red <= X"0";
+  Blue <= X"0";
 
 end Behavioral;
